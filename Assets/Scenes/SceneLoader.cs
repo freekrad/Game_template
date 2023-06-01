564a42941b61803749802c0689b04cb81f76c9b0
freekrad <evgeej@gmail.com>
2023-06-01 12:27:40 +0500

    Update SceneLoader.cs

--- Assets/Scenes/SceneLoader.cs
+++ Assets/Scenes/SceneLoader.cs
@@ -1,1 +1,17 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+public class ScenLoader : MonoBehaviour
+{
+  public string nextSceneName;
+  public GameObject progressBar;
+  public Image progressBarImage; // Спрайт полоски загрузки, на спрайте должна стоять галочка(заполняемый)***
+  public Text loadingText;
+  public GameObject pressKeyHint;
+  public GameObject loadingImage;
+  AsyncOporetion asyncOperation;
+  
+  
+}
